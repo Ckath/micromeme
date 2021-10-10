12602b27c6d3180ff6529f5dcf92ea3faa26d941
ckath <ckat@teknik.io>
2021-10-10 15:17:58 +0200

    update the id here

--- lib/usb_keyboard.c
+++ lib/usb_keyboard.c
@@ -42,8 +42,8 @@
 // Windows, even though the driver is supplied by Microsoft, an
 // INF file is needed to load the driver.  These numbers need to
 // match the INF file.
-#define VENDOR_ID		0x16C0
-#define PRODUCT_ID		0x047C
+#define VENDOR_ID		0xc81d
+#define PRODUCT_ID		0xc811
 
 
 // USB devices are supposed to implment a halt feature, which is
